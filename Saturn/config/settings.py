108c3d72db958924848a232de9897f1b351fa6fe
MrHalf <luyuhao627@gmail.com>
2016-02-20 19:27:45 -0500

    UI deploy
    Summary: Deploy settings for UI. And I would deploy to the server after you accepted
    
    Test Plan: No
    
    Reviewers: bbemis017
    
    Reviewed By: bbemis017
    
    Subscribers: dreamer, chen1631, zhayuhong, chen
    
    Differential Revision: http://phab.mrha1f.com/D5

--- Saturn/config/settings.py
+++ Saturn/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')x8c@9e&0z%2^o(*kt#g(5+&rnm)0rwwnt%15pc^v!0l$))-8@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '52.71.144.214']
 
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 try:
     from .local_settings import *
--- Saturn/config/urls.py
+++ Saturn/config/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from config import views
+
+
+extra_pattern = [
+    url(r'^$', views.home),
+]
 
 urlpatterns = [
+	url(r'^', include(extra_pattern)),
     url(r'^admin/', admin.site.urls),
 ]
