c7d8599f2e972df813993f07ed55a464b643fe6a
Siyang Chen <chen1564@purdue.edu>
2016-03-31 21:49:10 -0400

    Add course template page. I put the url in views.py and urls.py in accounts because i'm not sure where this should go for now
    Test Plan: no
    
    Reviewers: bbemis017
    
    Differential Revision: http://phab.mrha1f.com/D44

--- Saturn/accounts/urls.py
+++ Saturn/accounts/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^signout/$', views.signout),
     url(r'^files/$', views.files),
     url(r'^upload/$', views.upload),
+    url(r'^courseTemplate/$', views.courseTemplate),
 ]
