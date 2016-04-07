5578c17cb15149ee54741b4ceb9e864fdc3620f2
gundam1996 <whoami@pal-nat186-42-19.itap.purdue.edu>
2016-04-07 13:47:11 -0400

    separate the template creation in create site method
    Summary: separate template creation into another method
    
    Test Plan: none
    
    Reviewers: bbemis017
    
    Reviewed By: bbemis017
    
    Subscribers: dreamer, mrhalf
    
    Differential Revision: http://phab.mrha1f.com/D51

--- Saturn/website/views.py
+++ Saturn/website/views.py
@@ -44,8 +44,6 @@ def arrayExists(request,string):
     else:
         return False
 
-
-
 @login_required
 def createSite(request):
     #for information displayed on navigation bar
@@ -76,8 +74,7 @@ def createSite(request):
                     response_data['error'] = 1
                 if 'error' in response_data:
                     return JsonResponse(response_data)
-
-
+                 
                 domain = request.POST.get('domain')
                 title = request.POST.get('title')
                 author = request.POST.get('author')
@@ -86,14 +83,15 @@ def createSite(request):
                 sections = {} 
 
                 #check and assign variables
-                print request.POST
                 if arrayExists(request,'sections'):
                     sections = json.loads(request.POST.get('sections') )
                 if varExists(request,'summary'):
                     summary = request.POST.get('summary')
 
-               #check if site exists
+                #check if site exists
                 if not Website.objects.filter(domain=domain).exists():
+<<<<<<< HEAD
+=======
                     # creates a resume template by default
                     template = ResumeTemplate.objects.create(title=title)
                     template.path = "website/resumeTemplate.html"
@@ -105,61 +103,12 @@ def createSite(request):
                         summ.title = "About Me"
                         summ.content = summary
                         summ.save()
+>>>>>>> origin/master
                     
-                    #introduction
-                    introduction = Introduction.objects.create(user=request.user,template=template)
-                    save = False
-                    if varExists(request,'name'):
-                        introduction.title = request.POST.get('name') 
-                        save = True
-                    if varExists(request,'education'):
-                        introduction.education = request.POST.get('education') 
-                        save = True
-                    if arrayExists(request,'majors'):
-                        introduction.majors = request.POST.get('majors') 
-                        save = True
-                    if arrayExists(request,'languages'):
-                        introduction.languages = request.POST.get('languages')
-                        save = True
-                    if varExists(request,'gpa'):
-                        introduction.gpa = request.POST.get('gpa')
-                        save = True
-                    if save:
-                        introduction.classes += " gray-bg"
-                        introduction.save()
-                    else:
-                        introduction.delete()
-
-
-                    #create experience section
-                    save = False
-                    exp = Experience.objects.create(user=request.user,template=template)
-                    if arrayExists(request,'skills'):
-                        exp.skills = request.POST.get('skills')
-                        save = True
-                    if varExists(request,'experience'):
-                        exp.content = request.POST.get('experience')
-                        save = True
-                    if save:
-                        exp.title = "Experience"
-                        exp.save()
-                    else:
-                        exp.delete()
-
-
-                    #create sections
-                    section = None 
-                    print sections
-                    for i in range(len(sections)):
-                        if i % 2 == 0:
-                            section = Post.objects.create(user=request.user, template=template)
-                            section.title = sections[i]
-                            if i % 4 == 0:
-                                section.classes += " gray-bg"
-                        else:
-                            section.content = sections[i]
-                            section.save()
-
+                    template = ResumeTemplate.objects.create(title=title, description=description)
+                    introduction = Introduction.objects.create(user=request.user, template=template)
+                    exp = Experience.objects.create(user=request.user, template=template)
+                    create_resume_template(request, sections, template, introduction, exp)
 
                     #create Site
                     website = Website.objects.create(user=request.user,template=template)
@@ -191,8 +140,81 @@ def createSite(request):
 
     return render(request, "website/createSite.html",locals())
 
+<<<<<<< HEAD
+
+def create_resume_template(request, sections, template, introduction, exp):
+    print "what is going on"
+    #template = ResumeTemplate.objects.create(title=request.POST.get('title'),description=request.POST.get('description'))
+    template.path = "website/resumeTemplate.html"
+    template.author = request.POST.get('author')
+    template.save()
+
+    if varExists(request,'summary'):
+        summ = Post.objects.create(user=request.user,template=template)
+        summ.title = "About Me"
+        summ.content = request.POST.get('summary')
+        summ.save()
+                    
+    #introduction
+   # introduction = Introduction.objects.create(user=request.user,template=template)
+    save = False
+    if varExists(request,'name'):
+        introduction.title = request.POST.get('name') 
+        save = True
+    if varExists(request,'education'):
+        introduction.education = request.POST.get('education') 
+        save = True
+    if arrayExists(request,'majors'):
+        introduction.majors = request.POST.get('majors') 
+        save = True
+    if arrayExists(request,'languages'):
+        introduction.languages = request.POST.get('languages')
+        save = True
+    if varExists(request,'gpa'):
+        introduction.gpa = request.POST.get('gpa')
+        save = True
+    if save:
+        introduction.classes += " gray-bg"
+        introduction.save()
+    else:
+        introduction.delete()
+
+
+    #create experience section
+    save = False
+   # exp = Experience.objects.create(user=request.user,template=template)
+    if arrayExists(request,'skills'):
+        exp.skills = request.POST.get('skills')
+        save = True
+    if varExists(request,'experience'):
+        exp.content = request.POST.get('experience')
+        save = True
+    if save:
+        exp.title = "Experience"
+        exp.save()
+    else:
+        exp.delete()
+                        
+
+    #create sections
+    section = None 
+    for i in range(len(sections)):
+        if i % 2 == 0:
+            section = Post.objects.create(user=request.user, template=template)
+            section.title = sections[i]
+            if i % 4 == 0:
+                section.classes += " gray-bg"
+        else:
+            section.content = sections[i]
+            section.save()
+
+
+@login_required
+def selectTemplate(request):
+=======
 @login_required
 def selectTemplate(request):
     if 'error' in request.GET:
         template_error = True 
+>>>>>>> origin/master
     return render(request, "website/selectTemplate.html", locals())
