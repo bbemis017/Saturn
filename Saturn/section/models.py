24282cf8ac81c0ac6acb41755647bfd1374680c2
MrHalf <luyuhao627@gmail.com>
2016-04-09 02:19:22 -0400

    fix upload preview

--- Saturn/section/models.py
+++ Saturn/section/models.py
@@ -21,7 +21,7 @@ class Section(models.Model):
 
 
 def user_directory_path(instance, filename):
-    return 'static/user/{0}/{1}'.format(instance.user.id, filename)
+    return 'user/{0}/{1}'.format(instance.user.id, filename)
 
 
 class File(Section):
